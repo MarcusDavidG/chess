83017c5f926d67b7d70eb0e320fd3e55bb9f42f6
MarcusDavidG <davidmarcus827@gmail.com>
2026-02-22 04:16:00 +0100

    feat: add array utility functions

---
+++ src/lib/array-utils.ts
@@ -0,0 +1,1 @@
+export const arrayChunk = <T>(array: T[], size: number): T[][] => { const chunks = []; for (let i = 0; i < array.length; i += size) chunks.push(array.slice(i, i + size)); return chunks; }
